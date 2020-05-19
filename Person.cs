d6953538827ec485c9ed36e667e1726c31fa354b
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-19 15:42:58 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Validation_of_Data
+{
+    class Person
+    {
+        public Person(string firstname, string lastname, int age, decimal salary)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        private string firstname;
+        private string lastname;
+        private int age;
+        private decimal salary;
+
+        public string Firstname
+        {
+            get
+            {
+                return this.firstname;
+            }
+           set
+           {
+                if (value.Length<3)
+                throw new ArgumentException("First name should not be less than 3 characters ");
+                this.firstname = value;
+           }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return this.lastname;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Last name must not be blank");
+                this.lastname = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+             set
+             {            
+                if(value<=0)
+                throw new ArgumentException("Age cannot be zero or negative integer"); 
+                this.age = value;
+             }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+             set
+            {
+                if(value<460)
+                throw new ArgumentException("salary cannot be less than 460 dollar!");
+                this.salary = value;
+            }
+        }
+         public override string ToString()
+         {
+             return $"{this.firstname}  {this.lastname} receives  {this.salary} dollars";
+         }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Validation_of_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = 2;
+
+            var persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+
+            {
+
+                var cmdArgs = Console.ReadLine().Split();
+
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+
+            }
+
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Console.ReadLine();
+
+        }
+    }
+    
+}
